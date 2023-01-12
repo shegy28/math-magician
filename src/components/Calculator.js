29eb9fa86b27cd622a5c46c757c1f21069aa9ec3
shegy28 <shegy28@gmail.com>
2023-01-12 14:08:37 +0100

    Update to a functional component with hooks

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,66 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import calculate from '../logic/calculate';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-    };
+const Calculator = () => {
+  const [state, setState] = useState({ total: 0, next: null, operation: null });
+  const { total, next, operation } = state;
 
-    this.btnClicked = (e) => {
-      const clickedBtn = e.target.innerText;
-      const Result = calculate(this.state, clickedBtn);
-      this.setState(Result);
-    };
-  }
+  const btnClicked = (e) => {
+    const clickedBtn = e.target.innerText;
+    const gotValue = calculate(state, clickedBtn);
+    setState(gotValue);
+  };
 
-  render() {
-    const { total, operation, next } = this.state;
-    return (
-      <div className="calculator">
-        <div className="result">
-          <span>
-            {total}
-            {operation}
-            {next}
-          </span>
-        </div>
-        <div className="num">
-          <button type="button" onClick={this.btnClicked}>AC</button>
-          <button type="button" onClick={this.btnClicked}>+/-</button>
-          <button type="button" onClick={this.btnClicked}>%</button>
-          <button type="button" className="act" onClick={this.btnClicked}>÷</button>
-        </div>
-        <div className="num">
-          <button type="button" onClick={this.btnClicked}>7</button>
-          <button type="button" onClick={this.btnClicked}>8</button>
-          <button type="button" onClick={this.btnClicked}>9</button>
-          <button type="button" className="act" onClick={this.btnClicked}>x</button>
-        </div>
-        <div className="num">
-          <button type="button" onClick={this.btnClicked}>6</button>
-          <button type="button" onClick={this.btnClicked}>5</button>
-          <button type="button" onClick={this.btnClicked}>4</button>
-          <button type="button" className="act" onClick={this.btnClicked}>-</button>
-        </div>
-        <div className="num">
-          <button type="button" onClick={this.btnClicked}>3</button>
-          <button type="button" onClick={this.btnClicked}>2</button>
-          <button type="button" onClick={this.btnClicked}>1</button>
-          <button type="button" className="act" onClick={this.btnClicked}>+</button>
-        </div>
-        <div className="zero">
-          <button type="button" className="o" onClick={this.btnClicked}>0</button>
-          <button type="button" className="zb" onClick={this.btnClicked}>.</button>
-          <button type="button" className="zb act" onClick={this.btnClicked}>=</button>
-        </div>
-
+  return (
+    <div className="calculator">
+      <div className="result">
+        <span>
+          {total}
+          {' '}
+          {operation}
+          {' '}
+          {next}
+        </span>
       </div>
-    );
-  }
-}
+      <div className="num">
+        <button type="button" onClick={btnClicked}>AC</button>
+        <button type="button" onClick={btnClicked}>+/-</button>
+        <button type="button" onClick={btnClicked}>%</button>
+        <button type="button" className="act" onClick={btnClicked}>÷</button>
+      </div>
+      <div className="num">
+        <button type="button" onClick={btnClicked}>7</button>
+        <button type="button" onClick={btnClicked}>8</button>
+        <button type="button" onClick={btnClicked}>9</button>
+        <button type="button" className="act" onClick={btnClicked}>x</button>
+      </div>
+      <div className="num">
+        <button type="button" onClick={btnClicked}>6</button>
+        <button type="button" onClick={btnClicked}>5</button>
+        <button type="button" onClick={btnClicked}>4</button>
+        <button type="button" className="act" onClick={btnClicked}>-</button>
+      </div>
+      <div className="num">
+        <button type="button" onClick={btnClicked}>3</button>
+        <button type="button" onClick={btnClicked}>2</button>
+        <button type="button" onClick={btnClicked}>1</button>
+        <button type="button" className="act" onClick={btnClicked}>+</button>
+      </div>
+      <div className="zero">
+        <button type="button" className="o" onClick={btnClicked}>0</button>
+        <button type="button" className="zb" onClick={btnClicked}>.</button>
+        <button type="button" className="zb act" onClick={btnClicked}>=</button>
+      </div>
+
+    </div>
+  );
+};
 
 export default Calculator;
