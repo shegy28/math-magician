85ff1a9db8c20662917a02065e38929de2ed86d9
shegy28 <shegy28@gmail.com>
2023-01-17 17:05:53 +0100

    Import addded pages to app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Calculator from './components/Calculator';
+import Home from './components/Home';
+import Nav from './components/Navbar';
+import Quote from './components/Quote';
+
 
 class App extends React.Component {
   constructor(props) {
@@ -10,9 +15,14 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="App">
-        <Calculator />
-      </div>
+      <Router>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/calculator" element={<Calculator />} />
+          <Route path="/quote" element={<Quote />} />
+        </Routes>
+      </Router>
     );
   }
 }
